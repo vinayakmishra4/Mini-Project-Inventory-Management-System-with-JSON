fa19467afe67e93888f22defb9d9401dc686d431
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-09-17 08:51:01 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,22 @@
- # Marks src as a Python package
+"""
+Inventory Management System package.
+
+This package provides modules for managing products, categories,
+transactions, and inventory using JSON as storage.
+"""
+
+from . import inventory
+from . import utils
+from . import product_manager
+from . import category_manager
+from . import transaction_manager
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "inventory",
+    "utils",
+    "product_manager",
+    "category_manager",
+    "transaction_manager",
+]
