06cd9663d8b343d44417b58336987087013a4c8e
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-09-18 19:57:09 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,15 @@
 # Utility functions (JSON read/write etc.)
+import json  # Import the json module to work with JSON files
+
+# This function reads all the data from a JSON file
+def load_json(filename):
+    # Open the file in read mode ('r')
+    with open(filename, 'r') as file:
+        # Load the contents of the file into a Python object (usually a dict or list)
+        data = json.loads(file.read())
+
+    # (Optional) Return the loaded data so it can be used outside the function
+    return data
+
+
+def write_json(filename):
