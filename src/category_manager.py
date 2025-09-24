99318c2b2d0fc567568a6803f37114df4ab0797b
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-09-24 19:00:14 +0530

    Update category_manager.py

--- src/category_manager.py
+++ src/category_manager.py
@@ -1,1 +1,11 @@
 # Manage category operations
+import json
+import os
+import utils as ut
+
+CATEGORY_FILE = 'categories.json'
+DATA=os.path.join(ut.get_data_directory(), CATEGORY_FILE)
+
+# Load categories from the JSON file
+
+def get_all_categories():
